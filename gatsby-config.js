8f81d1f2b6e98e5d1fa5d0df70ef169be16301f6
Vincent Banagas <vabanagas@gmail.com>
2019-12-10 11:10:12 -0800

    Replace favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Whittier PONY Baseball`,
+        short_name: `WPB`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/pony-logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-typescript`,
