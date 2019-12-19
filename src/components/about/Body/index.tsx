d5d22a1b72760bb4970d1ec6080b5da1b57f65a9
Vincent Banagas <vabanagas@gmail.com>
2019-12-19 15:05:03 -0800

    Render body from markdown

---
+++ src/components/about/Body/index.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import styled from "styled-components"
+
+import breakpoints from "../../../constants/breakpoints"
+import values from "../../../constants/values"
+
+const Container = styled.div`
+  position: relative;
+  height: 100vh;
+  width: 100vw;
+  padding-top: ${values.HEADER_HEIGHT}px;
+`
+
+const Content = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 24px;
+
+  @media ${breakpoints.desktop} {
+    padding: calc(1 / 12 * 100vw);
+  }
+`
+
+export interface IBodyProps {
+  html: string
+}
+
+const Body = (props: IBodyProps) => (
+  <Container>
+    <Content dangerouslySetInnerHTML={{ __html: props.html }} />
+  </Container>
+)
+
+export default Body
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,6 +4,7 @@ import { get } from "lodash"
 
 import Layout from "../components/common/Layout"
 import SEO from "../components/common/SEO"
+import Body from "../components/about/Body"
 
 export const query = graphql`
   query AboutPage {
@@ -15,21 +16,15 @@ export const query = graphql`
   }
 `
 
-const renderBody = (body: React.ReactNode) => {
-  return body
-}
-
-const parseQuery = (data: object) =>
-  get(data, "file.childMarkdownRemark.frontmatter")
+const parseQuery = (data: object) => get(data, "file.childMarkdownRemark.html")
 
 const AboutPage = ({ data }: { data: object }) => {
   const result = parseQuery(data)
-  const body = get(result, "banner", null)
 
   return (
     <Layout lightContent={true}>
       <SEO title="About" />
-      {renderBody(body)}
+      <Body html={result} />
     </Layout>
   )
 }
